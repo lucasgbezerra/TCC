34488ebec77de5ebb8f2564321a6685875e39688
lucasgbezerra <lucasbsb100@gmail.com>
2024-06-21 00:09:27 -0300

    novidades no script

--- apiChatGPT.py
+++ apiChatGPT.py
@@ -2,34 +2,82 @@ from openai import OpenAI
 from dotenv import dotenv_values
 import os
 import json
+import re
 
 env_vars = dotenv_values(".env")
-key = env_vars.get("OPENAI_API_KEY")
+KEY = env_vars.get("OPENAI_API_KEY")
+PROJECT_ID = env_vars.get("PROJECT_ID")
 client = OpenAI(
-    api_key=key
+    api_key=KEY,
+    organization='org-hb4V3o4P9oJnavCMtYL4yWDi',
+    project=PROJECT_ID,
 )
 
-def enviar(texto):
 
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": texto},
-    ]
-    )
+raiz = "/home/lucas/tcc"
 
-    return completion.choices[0].message
+def createFile():
+    with open('arquivo.txt', 'w') as f:
+        f.write("Olá, tudo bem?")
+
+def getOpenAiResponse(prompt):
+
+    response = client.chat.completions.create(    
+        model="gpt-3.5-turbo",
+        messages=[
+         {"role": "user", "content": prompt},
+        ],)
+
+    return response.choices[0].message.content
 
 
-def ler_arquivo(nome_arquivo):
-    with open(nome_arquivo, 'r') as f:
+def readFile(nome):
+    with open(nome, 'r') as f:
         return f.read()
 
-def main():
-    nome_arquivo = 'arquivo.txt'  # Substitua pelo caminho do seu arquivo
-    texto = ler_arquivo(nome_arquivo)
-    resposta_gpt = enviar(texto)
-    print(resposta_gpt)
+def readJson(nome):
+    with open(nome, 'r') as f:
+        return json.load(f)
 
+def writeFile(fileName, content):
+    try:
+        with open(fileName, 'w', encoding='utf-8') as file:
+            file.write(content)
+    except Exception as e:
+        print("Erro: " + e)
+
+def readClass(filename):
+    result = ""
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            if not line.strip().startswith(('*', '/')):
+                result += line
+    return result
+
+def extractCode(response_text):
+    # Expressão regular para extrair blocos de código Java
+    java_code_pattern = re.compile(r'```java(.*?)```', re.DOTALL)
+    java_code_blocks = java_code_pattern.findall(response_text)
+    return "\n\n".join(java_code_blocks)
+
+
+def main():
+    template = readFile('template.txt')
+    infos = readJson('infos.json')
+    count = 0
+    # while count < len(infos):
+    while count < 1:
+        for item in infos:
+            # Escolhendo as melhores classes
+            classe = readClass(f"{item['path']}")
+            if len(classe.splitlines())/len(item['metodos']) < 30:
+                template = template.replace('CLASS_NAME', item['classe'])
+                template = template.replace('METHOD_NAME', ', '.join(item['metodos']))
+                prompt = f'{classe}\n\n{template}'
+                response = getOpenAiResponse(prompt)
+                javaCode = extractCode(response)
+                writeFile(f"{raiz}/responses/{item['classe']}Teste.java", javaCode)
+                break
+        count +=1
 if __name__ == "__main__":
     main()
--- buscaMetodos.py
+++ buscaMetodos.py
@@ -1,14 +1,15 @@
 import re
 import os
+import json
 
 raiz = '/home/lucas/tcc'
-caminhoProjeto = f'{raiz}/zaproxy/zap/src'
+dirProjeto = f'{raiz}/reps/zaproxy/zap/src'
+dirMetodos = f'{raiz}/metodos'
 lstClasses = []
-lstClasseReduzida = []
 
 def buscaClasse():
     count = 0
-    for pasta_raiz, sub_pastas, arquivos in os.walk(caminhoProjeto):
+    for pasta_raiz, sub_pastas, arquivos in os.walk(dirProjeto):
         for arquivo in arquivos:
             if arquivo.endswith(".java") and not "test" in arquivo.lower():
                 lstClasses.append({'classe': '','path': '','testePath': '', 'metodos': []})
@@ -19,12 +20,12 @@ def buscaClasse():
         
     for classe in lstClasses:
         arquivoJava = classe['classe'] + ".java"
-        for pasta_raiz, sub_pastas, arquivos in os.walk(caminhoProjeto):
+        for pasta_raiz, sub_pastas, arquivos in os.walk(dirProjeto):
             if arquivoJava in arquivos:
                 classe['path'] = os.path.join(pasta_raiz, arquivoJava)
  
 def buscaTeste():
-    for pasta_raiz, sub_pastas, arquivos in os.walk(caminhoProjeto):
+    for pasta_raiz, sub_pastas, arquivos in os.walk(dirProjeto):
         for arquivo in arquivos:
             if arquivo.endswith(".java") and "test" in arquivo.lower():
                 for classe in lstClasses:
@@ -42,11 +43,54 @@ def removeClassesSemCaminho():
 
     return novaLista
 
-def buscaMetodo():
-    ...
+# def extraiMetodo(classe, arquivo_path, metodos):
+#     with open(arquivo_path, 'r') as arquivo:
+#         arquivoTexto = arquivo.readlines()
+
+#     metodo_atual = None
+#     corpo_metodo = ""
+#     metodo_encontrado = False
+#     numChavesAbertas = 0
+#     padraoMetodo = re.compile(r'(?:(?:public|private|protected|static)\s+)+[\w\<\>\[\]]*\s*(\w+)\s*\([^\)]*\)\s*[^\{}]*{')
+
+#     for line in arquivoTexto:
+#         match =  padraoMetodo.search(line)
+#         if match:
+#             print(match.group(1) + "=====")
+#             if match.group(1) in metodos:
+#                 metodo_atual = match.group(1)
+#                 metodo_encontrado = True
+#                 corpo_metodo = ""
+#                 numChavesAbertas = 0
+                
+
+#         if metodo_encontrado:
+#             corpo_metodo += line
+#             print(metodo_atual + "-------" + numChavesAbertas.__str__())
+
+#             numChavesAbertas += line.count("{")
+#             numChavesAbertas -= line.count("}")
+            
+#             if numChavesAbertas == 0:
+#                 for metodo in metodos:
+#                     if metodo == metodo_atual:
+#                         criaArquivo(metodo, corpo_metodo, "java", dirMetodos, classe)
+#                         metodo_encontrado = False
+#                         break
+
+
+def criaArquivo(nome, conteudo,extensao, dir, subDir=""):
+    dir = os.path.join(dir, f"{dir}/{subDir}")
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    arquivoMetodo = os.path.join(dir, f"{nome}.{extensao}")
+    with open(arquivoMetodo, 'w') as arquivoSaida:
+        arquivoSaida.write(conteudo)
+        arquivoSaida.write("\n")
 
 def armazenaMetodo(lista):
-    for classe in lista:
+    for indice, classe in enumerate(lista, start=0):
         metodos = []
         if not os.path.exists(classe['path']):
             print(f"O arquivoTexto não existe.")
@@ -63,12 +107,9 @@ def armazenaMetodo(lista):
             if match:
                 assMetodo = match.group(1)
                 if not metodos.__contains__(assMetodo ) and len(assMetodo) > 1:
-                    metodos.append({"assinatura": assMetodo, "corpo": corpoMetodo})
+                    metodos.append(assMetodo)
 
         padraoTeste = r'\b(\w+)\.(\w+)\s*\([^)]*\)'
-        print(classe['classe'])
-        print(metodos
-              )
         with open(classe['testePath'], 'r') as f:
             linhas = f.readlines()
             for linha in linhas:
@@ -76,23 +117,29 @@ def armazenaMetodo(lista):
                 if match:
                     instancia = match.group(1)
                     metodo = match.group(2)
-                    print("---"+ metodo + "---")
                     if metodo in metodos and metodo not in classe['metodos']:
                         classe['metodos'].append(metodo)
+        
+        lista[indice]['metodos'] = classe['metodos']
+        # extraiMetodo(classe["classe"], classe['path'], classe['metodos'])
+
+def armazenaInfos(listaReduzida):
+    jsonData = json.dumps(listaReduzida)
+    criaArquivo("infos", jsonData, "json", raiz)
+
 def main():        
     buscaClasse()
     buscaTeste()
-    lstClasses = removeClassesSemCaminho()
-    armazenaMetodo(lstClasses)
-    for classe in lstClasses:
+    listaReduzida = removeClassesSemCaminho()
+    armazenaMetodo(listaReduzida)
+    for classe in listaReduzida:
         print(classe['classe'])
         print("PATH:" + classe['path'])
         print("TESTE: " + classe['testePath'])
         print(classe['metodos'])
         print("\n")
-    print(len(lstClasses))
-
-    
+    print(len(listaReduzida))
+    armazenaInfos(listaReduzida)
 
 
 if __name__ == "__main__":
